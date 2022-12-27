d912f65fa3a386c92befbf8e47c8766bb05f01a7
adityajai25 <adityajai243@gmail.com>
2022-12-28 02:10:29 +0530

    Question 1

--- sequence.py
+++ sequence.py
@@ -1,5 +1,3 @@
-# Question 1. Write a for loop to iterate through the list A = [1, 2, 3, 4, 5, 6]. Square each element of the list in one by one fashion and print them. After the end of the iteration, print - "The sequence has ended".
-
 A = [1,2,3,4,5,6]
 
 for element in A:
